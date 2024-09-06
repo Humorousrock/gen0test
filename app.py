c75268e40d714e605c4d7bd81708278e35cad378
Humorousrock <86119417+Humorousrock@users.noreply.github.com>
2024-09-06 17:08:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from vertexai.language_models import ChatModel
 import os
 
 app = Flask(__name__)
-PROJECT_ID = "tt-dev-001"  
+PROJECT_ID = "smart-impact-430905-t2"  
 LOCATION = "us-central1"  
 
 vertexai.init(project=PROJECT_ID, location=LOCATION)
